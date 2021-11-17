a93fcb1d874659f78c81010a55cbf4ff193a69ee
stanchula98 <stanchula98@gmail.com>
2021-11-17 14:03:09 +0100

    build

--- src/App.vue
+++ src/App.vue
@@ -43,6 +43,16 @@
 import DataTable from "./components/DataTable.vue";
 import metaData from "./parsed_data/Metadaten.json";
 import bloodsugar_json from "./parsed_data/Blutzucker.json";
+// const sqlite3 = require('sqlite3').verbose();
+
+// let db = new sqlite3.Database('db.db', sqlite3.OPEN_READWRITE, (err) => {
+//         if (err) {
+//           console.error(err.message);
+//         }
+//         console.log('Connected to the database.');
+//       });
+// console.log(sqlite3)
+
 
 var guid = [];
 
